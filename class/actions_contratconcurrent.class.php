a6f4beaddc7b5f2c15302d8149064cee8f6c08b5
phf <phf@atm-consulting.fr>
2016-09-08 09:10:23 +0200

    Fix rachat de ligne possible sauf dans le cas ou la ligne est présente dans une propal signé à facturer

--- class/actions_contratconcurrent.class.php
+++ class/actions_contratconcurrent.class.php
@@ -246,7 +246,7 @@ class ActionsContratConcurrent
 									SELECT pe.fk_contratdet_origin FROM '.MAIN_DB_PREFIX.'propaldet_extrafields pe 
 									INNER JOIN '.MAIN_DB_PREFIX.'propaldet pd ON (pd.rowid = pe.fk_object)
 									INNER JOIN '.MAIN_DB_PREFIX.'propal pl ON (pl.rowid = pd.fk_propal)
-									WHERE pl.fk_statut > 0
+									WHERE pl.fk_statut <> 2
 									)
 				#AND cd.statut = 4
 		';
